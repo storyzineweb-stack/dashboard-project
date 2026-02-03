e33a210fb468bf6ea1ef5034dc06ec2fc9bb603c
webstroyzine <storyzine.web@gmail.com>
2026-02-03 12:03:49 +0900

    dfbb
    fdgdgf

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import historyApiFallback from 'vite-plugin-history-api-fallback'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,9 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  server: {
+    historyApiFallback: true,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
